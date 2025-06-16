51b205418a078e72e77b031a7431c49cc6d88604
Dhe-Reigner <martinkagema01@gmail.com>
2025-06-16 04:57:48 +0300

    Initial token usage commit

--- app.py
+++ app.py
@@ -20,27 +20,44 @@
 
 
 
-# Memory
+# # Memory
+# from langchain_openai import OpenAI
+# from langchain.chains import ConversationChain
+# from langchain.memory import ConversationBufferMemory
+# from dotenv import load_dotenv
+# import os
+
+
+# load_dotenv()
+# api_key = os.getenv("OPENAI_API_KEY")
+
+# llm = OpenAI(model="gpt-3.5-turbo-instruct",temperature=0.7, openai_api_key = api_key)
+
+# conversation = ConversationChain(
+#     llm = llm,
+#     verbose = True,
+#     memory = ConversationBufferMemory()
+# )
+
+# conversation.predict(input = "Tell me about yourself. ")
+# conversation.predict(input = "What can you do? ")
+# conversation.predict(input = "How can you help me with data analysis? ")
+
+# print(conversation)
+
+
+# Token Usage
 from langchain_openai import OpenAI
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
+from langchain_community.callbacks import get_openai_callback
 from dotenv import load_dotenv
 import os
-
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
-llm = OpenAI(model="gpt-3.5-turbo-instruct",temperature=0.7, openai_api_key = api_key)
+llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0.7, openai_api_key = api_key)
 
-conversation = ConversationChain(
-    llm = llm,
-    verbose = True,
-    memory = ConversationBufferMemory()
-)
+with get_openai_callback() as cb:
+    results = llm.invoke("Tell me a poem about my girlfriend")
+    print(cb)
 
-conversation.predict(input = "Tell me about yourself. ")
-conversation.predict(input = "What can you do? ")
-conversation.predict(input = "How can you help me with data analysis? ")
-
-print(conversation)
