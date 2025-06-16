1c607d46cf9d60b2810b6df0338b35e5208544b4
Dhe-Reigner <martinkagema01@gmail.com>
2025-06-16 05:02:30 +0300

    Movie Summarizer

--- app.py
+++ app.py
@@ -63,55 +63,84 @@
 
 
 
-#  Few-Shot Learning
-from langchain_core.prompts.few_shot import FewShotPromptTemplate
-from langchain_core.prompts.prompt import PromptTemplate
-from langchain.chains import LLMChain
-from langchain_openai import OpenAI
+# #  Few-Shot Learning
+# from langchain_core.prompts.few_shot import FewShotPromptTemplate
+# from langchain_core.prompts.prompt import PromptTemplate
+# from langchain.chains import LLMChain
+# from langchain_openai import OpenAI
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+# api_key = os.getenv("OPENAI_API_KEY")
+
+# llm = OpenAI(model="gpt-3.5-turbo-instruct",temperature=0.7, openai_api_key = api_key)
+
+# examples = [
+#     {
+#         "query":"What is the weather like today?",
+#         "answer":"It is likely to rain cats and dogs, better carry an umbrella"
+#     },
+#     {
+#         "query":"What's your age?",
+#         "answer":"Age is just a number, but I am timeless"
+#     }
+# ]
+# example_template = """
+# User:{query}
+# AI:{answer}
+# """
+
+# example_prompt = PromptTemplate(
+#     input_variables=["query", "answer"],
+#     template = example_template
+# )
+# prefix = """
+# The following are excerpts from conversations with an AI
+# assistant. The assistant is known for its humor and wit, providing
+# entertaining and amusing responses to users' questions. Here are some
+# examples:
+# """
+# suffix = """
+# User:{query}
+# AI:
+# """
+# few_shot_prompt_template = FewShotPromptTemplate(
+#     examples = examples,
+#     example_prompt = example_prompt,
+#     prefix = prefix,
+#     suffix = suffix,
+#     input_variables = ["query"],
+#     example_separator = "\n\n"
+# )
+# chain = few_shot_prompt_template | llm
+# print(chain.invoke("Some sweet words for my girlfriend?"))
+
+
+#  Movie Summarizer
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate
+)
 from dotenv import load_dotenv
 import os
+
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
-llm = OpenAI(model="gpt-3.5-turbo-instruct",temperature=0.7, openai_api_key = api_key)
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.7, openai_api_key = api_key)
 
-examples = [
-    {
-        "query":"What is the weather like today?",
-        "answer":"It is likely to rain cats and dogs, better carry an umbrella"
-    },
-    {
-        "query":"What's your age?",
-        "answer":"Age is just a number, but I am timeless"
-    }
-]
-example_template = """
-User:{query}
-AI:{answer}
-"""
+template  = "You are a helpful movie summarizer, giving attention to details"
+system_prompt = SystemMessagePromptTemplate.from_template(template)
 
-example_prompt = PromptTemplate(
-    input_variables=["query", "answer"],
-    template = example_template
-)
-prefix = """
-The following are excerpts from conversations with an AI
-assistant. The assistant is known for its humor and wit, providing
-entertaining and amusing responses to users' questions. Here are some
-examples:
-"""
-suffix = """
-User:{query}
-AI:
-"""
-few_shot_prompt_template = FewShotPromptTemplate(
-    examples = examples,
-    example_prompt = example_prompt,
-    prefix = prefix,
-    suffix = suffix,
-    input_variables = ["query"],
-    example_separator = "\n\n"
-)
-chain = few_shot_prompt_template | llm
-print(chain.invoke("Some sweet words for my girlfriend?"))
+human_template = "Generate information about {movie_title}"
+human_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+chat_prompt = ChatPromptTemplate([system_prompt, human_prompt])
+
+response = llm.invoke(chat_prompt.format_prompt(movie_title="god's must be crazy").to_messages())
+
+print(response.content)
